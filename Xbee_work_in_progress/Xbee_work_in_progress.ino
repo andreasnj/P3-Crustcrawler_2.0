26fec5054ea893c6d73f468b6ec0e65e7da97c67
Andreas Nellemann Jørgensen <andr544e@gmail.com>
2017-10-31 13:02:05 +0100

    New Packet reading file
    Not the one Jens put up, but trying to understand it from scratch

---
+++ Xbee_work_in_progress/Xbee_work_in_progress.ino
@@ -0,0 +1,101 @@
+/*
+   Format for XBEE packets
+    0 0x7e  - start ch
+    1 0x00  - length msb
+    2 0x14  - length lsb
+    3 0x83  - API frame identifier
+    4 0x56  - senders address
+    5 0x78  - senders address
+    6 0x43  - received signal strength - RSI
+    7 0x00  - option byte
+    8 0x01  - nu,mber of samples
+    9 0x3e  - channel indicator mask n/a A5 A4 A3 A2 A1 A0 D8
+   10 0xe0  - channel indicatot mask  D7 D6 D5 D4 D3 D2 D1 D0
+   11 0x00  - digital sample (msb)
+   12 0x40  - digital sample (lsb)
+
+   13 0x??  - Acc Z (msb)
+   14 0x??  - Acc Z (lsb)
+
+   15 0x??  - Acc Y (msb)
+   16 0x??  - Acc Y (lsb)
+
+   17 0x??  - Acc X (msb)
+   18 0x??  - Acc X (lsb)
+
+   19 0x??  - EMG ch1 (msb)
+   20 0x??  - EMG ch1 (lsb)
+
+   21 0x??  - EMG ch2 (msb)
+   22 0x??  - EMG ch2 (lsb)
+
+   23 0x??  - checksum
+
+   The checksum is all bytes added together excl the first three. The sum including(!) the checksum shall give 0xff
+   Strategy sum all bytes excluding first three. The checksum will be 0xff - sum
+*/
+char testPkg[24] = {0x7e, 0x00, 0x14, 0x83, 0x56, 0x78, 0x43, 0x00, 0x01, 0x3e,
+                    0xe0, 0x00, 0x40, 0x02, 0x9b, 0x02, 0x1a, 0x02, 0x05, 0x00,
+                    0x00, 0x00, 0x05, 0x47};  // from datasheet
+
+char hexTable[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+
+void setup() {
+  Serial.begin(115200);
+  while (! Serial) // wait until serial port is up and running
+  { }
+
+  delay(500); // just to be sure
+}
+
+char chksum(char *pk, int lgt)
+{
+  char s = 0;
+  for (int i = 0; i < lgt; i++)
+    s += *(pk + i);
+  return s;
+}
+
+void getPkg(char *package){
+  int counter = 0;
+  char temp;
+    do{
+      if(Serial.available()){
+        temp = Serial.read(); // reads the first element in serial buffer
+          if(temp == 0x7e){
+            package[counter] = temp;
+            counter++;
+            }
+      }
+    }while(counter == 0);
+    do{
+      if(Serial.available()){
+        temp = Serial.read();
+        package[counter] = temp;
+        counter++;
+      }
+      
+    }while(counter < 24);
+  
+  }
+
+int checksumPkg(char *package){
+  char sum;
+  for(int i = 3; i < 24; i++){
+    sum = sum + package[i];
+  }
+  if(sum == 0x47){ //Check with the checksum of the package
+    return 0; // ok, good package
+  }
+  else{
+    return -1; // error
+  }
+}
+  
+
+
+
+void loop() {
+  delay(5000);
+}
