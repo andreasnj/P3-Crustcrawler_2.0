93cd41eb20125cceeb3ed6c83b2a2a2b95b9e107
Elona <eloniuke@gmail.com>
2017-11-02 12:43:54 +0100

    xBee library

--- libraries/xBee/readingxBee.h
+++ libraries/xBee/readingxBee.h
@@ -8,7 +8,7 @@
 #include <pins_arduino.h>
 #endif
 
-#define START                       0x7e
+#define START                       0x7E
 #define MSB                         0x00
 #define LSB                         0x14
 #define API_FRAME_ID                0x83
@@ -17,24 +17,42 @@
 #define RSI                         0x43
 #define OPTION_BYTE                 0x00
 #define SAMPLES_NUMBER              0x01
-#define CHANNEL_INDICATOR_H         0x3e //channel indicator mask n/a A5 A4 A3 A2 A1 A0 D8
-#define CHANNEL_INDICATOR_L         0xe0 //channel indicatot mask  D7 D6 D5 D4 D3 D2 D1 D0
+#define CHANNEL_INDICATOR_H         0x3E //channel indicator mask n/a A5 A4 A3 A2 A1 A0 D8
+#define CHANNEL_INDICATOR_L         0xE0 //channel indicatot mask  D7 D6 D5 D4 D3 D2 D1 D0
 #define DIGITAL_SAMPLE_H            0x00
 #define DIGITAL_SAMPLE_L            0x40
 
-#define ACCZ_H                      0x??
-#define ACCZ_L                      0x??
+#define ACCZ_H                      0x02
+#define ACCZ_L                      0x9B
 
-#define ACCY_H                      0x??
-#define ACCY_L
+#define ACCY_H                      0x02
+#define ACCY_L                      0x1A
 
-#define ACCX_H                      0x??
-#define ACCX_L                      0x??
+#define ACCX_H                      0x02
+#define ACCX_L                      0x05
 
-#define EMGCH1_H                    0x??
-#define EMGCH1_L                    0x??
+#define EMGCH1_H                    0x00
+#define EMGCH1_L                    0x00
 
-#define EMGCH2_H                    0x??
-#define EMGCH2_L                    0x??
+#define EMGCH2_H                    0x00
+#define EMGCH2_L                    0x05
 
-#define CHECKSUM                    0x??
+#define CHECKSUM                    0x47
+
+class xBeeClass {
+public:
+    //constructor
+    xBeeClass(): {} //not sure what should go here
+
+private:
+    void readPacket();
+    void printReturn();
+
+    unsigned char packet_array[24];
+};
+
+extern xBeeClass xBee;
+
+#endif
+
+}
