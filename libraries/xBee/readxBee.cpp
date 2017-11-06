f60d8d31cfc8bd5d469f340445d2f1805a5cbf8d
Andreas Nellemann Jørgensen <andr544e@gmail.com>
2017-11-06 16:23:11 +0100

    --

--- libraries/xBee/readxBee.cpp
+++ libraries/xBee/readxBee.cpp
@@ -64,7 +64,7 @@ bool xBeeClass::checkPacket(char *pk){//Generates checksum and compares with the
   for(int i = 3; i < 23; i++){       //Generate sum from index 3-to-22 (until, not incl. the checksum itself)
     sum = sum + pk[i];
   }
-
+  
   if((0xff - sum) == pk[23]){        //Check with the checksum of the package
 
     return 1;                        //ok, good package
