6baa481ddff8332bcb55c4c7a5c5fa96f527d27b
Andreas Nellemann Jørgensen <andr544e@gmail.com>
2017-11-16 11:23:38 +0100

    --

--- ModifiedForMega/Pickup_sponge_Mega/Pickup_sponge_Mega.ino
+++ ModifiedForMega/Pickup_sponge_Mega/Pickup_sponge_Mega.ino
@@ -1,4 +1,20 @@
--//This program should be run to set the servos to some standard initial positions. Works with the Uno
+//This program should be run to set the servos to some standard initial positions. Works with the Mega
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// How to run it: upload to arduino with the dynamixel_serial library installed in your arduino libraries folder.             //
+//                After upload, disconnect usb from arduino and add power to CrustCrawler and the arduino board.              //
+//                The program should start by it self. :)
+//                                                                                                                            //
+//                PIN Setup:  Green wire to PIN 10,                                                                           //
+//                            Yellow wire to PIN 11,                                                                          //
+//                            Black wire to ground,                                                                           //
+//                            Red wire to 5v,                                                                                 //
+//                            Blue wire to PIN2                                                                               //
+//                                                                                                                            //
+//                This code was developed in collaboration with several groups, to enable all the groups a good               //
+//                base code to start programming the CrustCrawler from. ;)                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 #include "Dynamixel_Serial.h"
 #include <SoftwareSerial.h>
