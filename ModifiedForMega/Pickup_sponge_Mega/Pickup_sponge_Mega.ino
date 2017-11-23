7df37498ac502a2f28857005d8f44426f42a5f50
Andreas Nellemann Jørgensen <andr544e@gmail.com>
2017-11-23 16:42:59 +0100

    --

--- ModifiedForMega/Pickup_sponge_Mega/Pickup_sponge_Mega.ino
+++ ModifiedForMega/Pickup_sponge_Mega/Pickup_sponge_Mega.ino
@@ -19,7 +19,7 @@
 #include "Dynamixel_Serial.h"
 
 #define SERVO_ControlPin 0x02       // Control pin of buffer chip, NOTE: this does not matter becasue we are not using a half to full contorl buffer.
-#define SERVO_SET_Baudrate 57600    // Baud rate speed which the Dynamixel will be set too (57600)
+#define SERVO_SET_Baudrate 57600    // Baud rate speed which the Dynamixel will be set to (57600)
 #define LED13 0x0D
 #define CW_LIMIT_ANGLE 0x001        // lowest clockwise angle is 1, as when set to 0 it set servo to wheel mode
 #define CCW_LIMIT_ANGLE 0xFFF       // Highest anit-clockwise angle is 0XFFF, as when set to 0 it set servo to wheel mode
