5c91663b11d0dfd5ee0cabfdc4689ea137c5b373
Andreas Nellemann Jørgensen <andr544e@gmail.com>
2017-11-28 14:45:16 +0100

    Trying hardwareSerial
    Didn't work.

--- ModifiedForMega/Pickup_sponge_Mega/Pickup_sponge_Mega.ino
+++ ModifiedForMega/Pickup_sponge_Mega/Pickup_sponge_Mega.ino
@@ -1,4 +1,5 @@
-//This program should be run to set the servos to some standard initial positions. Works with the Mega
+//This program should be run to set the servos to some standard initial positions.
+//USE ONLY WITH MEGA
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // How to run it: upload to arduino with the dynamixel_serial library installed in your arduino libraries folder.             //
 //                After upload, disconnect usb from arduino and add power to CrustCrawler and the arduino board.              //
@@ -17,7 +18,7 @@
 
 
 #include "Dynamixel_Serial.h"
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 
 #define SERVO_ControlPin 0x02       // Control pin of buffer chip, NOTE: this does not matter becasue we are not using a half to full contorl buffer.
 #define SERVO_SET_Baudrate 57600    // Baud rate speed which the Dynamixel will be set too (57600)
@@ -27,21 +28,22 @@
 #define OPEN 'o'
 #define CLOSE 'c'
 
-SoftwareSerial mySerial(10, 11);    // RX, TX
+//SoftwareSerial mySerial(10, 11);    // RX, TX
 
 void setup() {
-  Serial.flush();                                       // Clear the serial buffer of garbage data before running the code.
-  mySerial.begin(SERVO_SET_Baudrate);                   // We now need to set Ardiuno to the new Baudrate speed 115200
-  Serial.begin(SERVO_SET_Baudrate);                                  // Start serial communication on baudrate 57600
-  Dynamixel.begin(mySerial);                            // Calling mySerial function which sets 10 pin as the 2nd RX serial pin, and sets pin 11 as the 2nd TX serial pin
-  Dynamixel.setDirectionPin(SERVO_ControlPin);          // Optional. Set direction control pin which controls if the program writes or reads
+  Serial1.begin(SERVO_SET_Baudrate);                   // Set Ardiuno to the new Baudrate speed 1152
+  Serial.begin(SERVO_SET_Baudrate);                    // Start serial communication on baudrate 57600
+  Serial.flush();                                      // Clear the serial buffer of garbage data.
+  Serial1.flush();
+  Dynamixel.begin(Serial1);                            // Calling mySerial function which sets 10 pin as the 2nd RX serial pin, and sets pin 11 as the 2nd TX serial pin
+  Dynamixel.setDirectionPin(SERVO_ControlPin);         // Optional. Set direction control pin which controls if the program writes or reads
 
-  //Turn on hold for the servos, so they stay in place
-  Dynamixel.setHoldingTorque(0x01, true);               //Set on hold turque for each servo
-  Dynamixel.setHoldingTorque(0x02, true);
-  Dynamixel.setHoldingTorque(0x03, true);
-  Dynamixel.setHoldingTorque(0x04, true);
-  Dynamixel.setHoldingTorque(0x05, true);
+  //Turn off hold, so we can modify EEPROM
+  Dynamixel.setHoldingTorque(0x01, false);               //Set on hold torque for each servo
+  Dynamixel.setHoldingTorque(0x02, false);
+  Dynamixel.setHoldingTorque(0x03, false);
+  Dynamixel.setHoldingTorque(0x04, false);
+  Dynamixel.setHoldingTorque(0x05, false);
 
   //Set profile acceleration
   Dynamixel.setProfileAcceleration(0x01, 10);           //Set profile acc for each servo
@@ -72,8 +74,7 @@ void loop() {
   Dynamixel.performMovement(2148, 1850, 3250, OPEN);     //Ready position
   delay(2000);
 
-
-  Dynamixel.performMovement(1600, 2800, 3200, OPEN);      //Move to sponge. 
+  Dynamixel.performMovement(1600, 2800, 3200, OPEN);      //Move to sponge
   Dynamixel.gripper(CLOSE);                               //Grab
   Dynamixel.performMovement(2148, 1850, 3250, CLOSE);     //Detour
   Dynamixel.performMovement(2650, 2800, 3200, CLOSE);     //Move to destination
